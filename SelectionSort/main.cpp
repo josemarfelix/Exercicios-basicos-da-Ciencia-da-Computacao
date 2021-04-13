6e25ef828c55d04882e384734d2b779352cc137b
Josemar Felix <67971880+josemarfelix@users.noreply.github.com>
2021-04-13 15:50:57 -0300

    Update main.cpp

--- SelectionSort/main.cpp
+++ SelectionSort/main.cpp
@@ -1,51 +1,46 @@
-
-// C++ program for implementation of selection sort
-#include <bits/stdc++.h>
 using namespace std;
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <bits/stdc++.h>
+#include <algorithm>
 
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
 
-void selectionSort(int arr[], int n)
+
+int main()
 {
+    // Create a vector containing integers
+    vector<int> v;
+    for(int i=0;i<INT_MAX;i++){
+        int valorvetor;
+        cin>>valorvetor;
+        // Adiciona os valores no vetor
+        v.push_back(valorvetor);
+        if(valorvetor<0)break;
+    }
+
+
+    // Print out the vector
+    cout << "v = { ";
+    for (int n : v) {
+        cout << n << ", ";
+    }
+    cout << "}; \n";
+    int n= v.size();
     int i, j, min_idx;
-
     // One by one move boundary of unsorted subarray
     for (i = 0; i < n-1; i++)
     {
         // Find the minimum element in unsorted array
         min_idx = i;
         for (j = i+1; j < n; j++)
-        if (arr[j] < arr[min_idx])
+        if (v[j] < v[min_idx])
             min_idx = j;
 
         // Swap the found minimum element with the first element
-        swap(&arr[min_idx], &arr[i]);
+        swap(v[min_idx], v[i]);
     }
-}
-
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        cout << arr[i] << " ";
+    for (i=0; i < n; i++)
+        cout << v[i] << " ";
     cout << endl;
 }
-
-// Driver program to test above functions
-int main()
-{
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    selectionSort(arr, n);
-    cout << "Sorted array: \n";
-    printArray(arr, n);
-    return 0;
-}
-
-// This is code is contributed by rathbhupendra
