f16c5b7c3d5869c1a9af4d66d5e201cc058f8ec4
Josemar Felix <67971880+josemarfelix@users.noreply.github.com>
2021-04-26 23:12:27 -0300

    Add files via upload

---
+++ BFS for Graph/cmake-build-debug/CMakeFiles/BFS_for_Graph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BFS_for_Graph.exe"
+  "BFS_for_Graph.exe.manifest"
+  "BFS_for_Graph.pdb"
+  "CMakeFiles/BFS_for_Graph.dir/main.cpp.obj"
+  "libBFS_for_Graph.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BFS_for_Graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
